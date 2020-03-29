a0c5cfacf507675399844eb79e38e0e6e3ac6077
Caio Reis <caioreis3186@gmail.com>
2020-03-29 01:00:42 -0300

    Mobile da aplicação

---
+++ mobile/src/pages/Detail/styles.js
@@ -0,0 +1,80 @@
+import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: 24,
+        paddingTop: Constants.statusBarHeight + 20,
+    },
+
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+
+    incident: {
+        padding: 24,
+        borderRadius: 8,
+        backgroundColor: '#fff',
+        marginBottom:16,
+        marginTop: 48,
+    },
+
+    incidentProperty: {
+        fontSize: 14,
+        color:'#41414d',
+        fontWeight: 'bold',
+        marginTop: 24,
+    },
+
+    incidentValue: {
+        marginTop: 8,
+        fontSize: 15,
+        color: '#737380',
+    },
+
+    contactBox: {
+        padding: 24,
+        borderRadius: 8,
+        backgroundColor: '#fff',
+        marginBottom:16,
+    },
+
+    heroTitle: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        color: '#13131a',
+        lineHeight: 30,
+    },
+
+    heroDescription: {
+        marginTop: 16,
+        fontSize: 15,
+        color: '#737380',
+    },
+
+    actions: {
+        flexDirection: 'row',
+        marginTop: 16,
+        justifyContent :'space-between',
+    },
+
+    action: {
+        borderRadius: 8,
+        backgroundColor: '#e02041',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 50,
+        width: '48%',
+    },
+    actionText:{
+        color: '#fff',
+        fontSize: 15,
+        fontWeight: 'bold'
+    }
+
+        
+});
